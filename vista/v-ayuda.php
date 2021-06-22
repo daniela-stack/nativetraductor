796c33fad518d15f7e1c0c97eac0ebb9f8f01ee9
lizbethcaro <81368772+lizbethcaro@users.noreply.github.com>
2021-06-22 07:48:46 -0500

    modificación de v-ayuda.php
    se actualiza los datos que se verán en el enlace de ayuda

--- vista/v-ayuda.php
+++ vista/v-ayuda.php
@@ -7,7 +7,28 @@
       <div class="row">
         <div class="span3">
           <h2>¿Preguntas Frecuentes?</h2>
-           <p>........ </p>
+          <li><p><b>1. Tengo que comprar la aplicación móvil?</b></p></a></li>
+            <p>No. Descargar e instalar nuestra aplicacion móvil es completamente gratuito.</p>
+
+            <li><p><b>2. Necesito iniciar sesion o registrarme para usar la aplicacion o para recibir ofertas especiales?</b></p></a></li>
+            <p>Por el momento no, no alamcenamos sus datos o información en ningun lugar.</p>
+
+            <li><p><b>3. mi telefono es compatible?</b></p></a></li>
+            <p>Si instalaste y abriste esta aplicación, entonces sí lo es.</p>
+
+            <li><p><b>4. Qué caracteristicas tiene la aplicacion móvil?</b></p></a></li>
+            <p>Por el momento, solo traduce la escritura.</p>
+
+            <li><p><b>5. Está disponible el modo de acceso fuerea de línea?</b></p></a></li>
+            <p>Si, el acceso fuera de linea está disponible para traducir la escritura. </p>
+
+            <li><p><b>6. Cómo se utiliza el traductor de escritura?</b></p></a></li>
+            <p>Solo tienes que ir al area de escritura y escribir tantas palabras o frases como necesites. Despues de esto sólo tendrás que hacer clic en traducir.
+            La traduccion se realizara de manera instantanea y eficiente.</p>
+
+            <li><p><b>7. Como puedo dar mi opinion sobre la aplicación?</b></p></a></li>
+            <p>Siempr eescuchamos a nuestros clientes, por eso creamos la seccion de comentarios y opinion, solo elige una de nuetras opciones o Escribe
+            tu propia opinion y da un clic en la boton de Enviar y listo.</p>
           <p><a class="btn" href="#">View details &raquo;</a></p>
         </div>
         <div class="span3">
@@ -22,7 +43,21 @@
         </div>
         <div class="span3">
           <h2>Comentarios</h2>
-          <p>......</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
+          <input type="radio" name="radio2"><b>Las traducciones son incorrectas a veces</input>
+          <br>
+          <input type="radio" name="radio2">Algunas funciones no funcionan</input>
+          <br>
+          <input type="radio" name="radio2">la aplicacion es lenta</input>
+          <br>
+          <input type="radio" name="radio2">Incluyan mas Idiomas</input>
+          <br>
+          <input type="radio" name="radio2">Escribe tu propia opinion</b></input>
+          
+                  
+                    <input type="text" name="palabra">
+                    <br>  
+                  
+          <input type="submit" value="Enviar" >
+          <!--<p><a class="btn" href="#">View details &raquo;</a></p>-->
         </div>
       </div>
